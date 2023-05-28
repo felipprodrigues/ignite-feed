9c4732cf30485d60fc566efb11860bd302a615e3
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-05-28 19:08:26 -0300

    initial commit

---
+++ app/src/App.jsx
@@ -0,0 +1,18 @@
+import { Post } from './Post.jsx'
+
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Felipe Rodrigues"
+        content="lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+      />
+      <Post
+        author="Jorge Cabron"
+        content="lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+      />
+    </div>
+  )
+}
+
+
