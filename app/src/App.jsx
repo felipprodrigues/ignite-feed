6b2d307186237640499f71aa931adf16a7d34b3f
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-05-29 01:35:01 -0300

    add several new elements

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,16 +1,30 @@
 import { Post } from './Post.jsx'
+import './global.css'
+import styles from './components/App.module.css'
+import {Header} from './components/Header.jsx'
+import {Sidebar} from './components/Sidebar.jsx'
 
 export function App() {
   return (
     <div>
-      <Post
-        author="Felipe Rodrigues"
-        content="lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
-      />
-      <Post
-        author="Jorge Cabron"
-        content="lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
-      />
+      <Header/>
+
+      <div className={styles.wrapper}>
+        <Sidebar/>
+
+        <main>
+          <Post
+            author="Felipe Rodrigues"
+            content="lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+          />
+          <Post
+            author="Jorge Cabron"
+            content="lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+          />
+
+        </main>
+      </div>
+
     </div>
   )
 }
